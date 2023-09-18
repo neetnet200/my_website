521e460fc0f1807a61d0a3e1269862e9d810f8bb
Anita de San Miguel <91314936+neetnet200@users.noreply.github.com>
2023-09-18 13:09:40 +1000

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+//
+ import Navbar from "./components/Navbar";
+ import Hero from "./components/Hero";
+ import About from "./components/About";
+ import Showcase from "./components/Showcase";
+import Footer from "./components/Footer";
+ import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/showcase" element={<Showcase />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/About.js
@@ -0,0 +1,49 @@
+import React from "react";
+import "./About.css";
+
+import { useNavigate } from "react-router-dom";
+import { TiVendorAndroid } from "react-icons/ti";
+
+function About() {
+  let navigate = useNavigate();
+  return (
+    <div className="about" id="about">
+      <div className="container">
+        <div className="col-1">
+          <div className="card">
+            <div className="image-wrap">
+            
+            </div>
+          </div>
+        </div>
+        <div className="col-2">
+          <h2>About</h2>
+          <div className="col-2-container">
+            
+            <p>
+            <TiVendorAndroid size={22} />  Frontend design + web development
+            <br />
+            <TiVendorAndroid size={22} />  React JS in an enterprise setting
+            <br />
+            <TiVendorAndroid size={22} />  Degree in Graphic Design
+            <br />
+            <TiVendorAndroid size={22} />  Experience with AWS Management Console
+            <br />
+            <TiVendorAndroid size={22} />  Node JS / Typescript
+            </p>
+          </div>
+          {/* <button
+            className="button"
+            onClick={() => {
+              navigate("/Showcase");
+            }}
+          >
+            Examples of work
+          </button> */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
